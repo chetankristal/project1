1ab6c6a6a9f3479138efa9815bfe04ca2ef29a71
Ubuntu <ubuntu@ip-10-0-1-79>
2023-09-11 16:42:52 +0000

    adding deployment files for docker swarm

---
+++ scripts/ansible_playbooks/files/staging/deploy_node.sh
@@ -0,0 +1,6 @@
+docker network create   --scope=swarm   --driver=overlay   --subnet=20.0.37.0/24   --gateway=20.0.37.1   my-overlay
+
+docker stack deploy -c docker_stack_node.yaml --with-registry-auth staging-frontend
+#sleep 5
+#docker system prune -f
+
